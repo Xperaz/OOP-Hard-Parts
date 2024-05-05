077a4d2799750eeddaa7131d67d19ffd62d7595d
azedine ouhadou <xperazazedine@gmail.com>
2024-05-05 10:48:11 +0100

    remove html file

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
-class userCreator {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-  }
+const user1 = {
+  name: "Phil",
+  score: 4,
+  // increment functionality to increase the score
+  increment: function () {
+    user1.score++;
+  },
+};
 
-  incrementScore() {
-    this.score++;
-  }
-
-  sayName() {
-    console.log("hello", this.name);
-  }
-}
-
-// const user1 = new userCreator("azedine", 10);
-// user1.sayName();
-// user1.incrementScore();
-// console.log(user1.name);
-
-// paidUser
-
-class paidUserCreator extends userCreator {
-  constructor(paidName, score, accountBalance) {
-    super(paidName, score);
-    this.accountBalance = accountBalance;
-  }
-
-  incrementBalance() {
-    this.accountBalance++;
-  }
-}
-
-const paidUser1 = new paidUserCreator("xperaz", 100, 250);
-paidUser1.sayName();
-paidUser1.incrementScore();
-paidUser1.incrementBalance();
-
-console.log(paidUser1);
+console.log(user1); // { name: 'Phil', score: 4, increment: [Function: increment] }
